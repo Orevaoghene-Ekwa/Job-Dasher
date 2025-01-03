b50391c6ea9ca47a42973a08554dd37448919ff4
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2025-01-03 10:00:58 +0100

    implemented flex box

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,13 +6,13 @@ import { useAuth, logout } from '../auth'
 const Footer =()=>{
 
     return ( 
-        <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+        <Footer className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
             <div className="container-fluid">
                 <footer class="bg-dark text-white-50">
                 <span>©Job Dasher.</span>
             </footer>
             </div>
-        </nav>
+        </Footer>
     )
 }
 
