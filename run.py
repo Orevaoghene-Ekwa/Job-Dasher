aa6973b57b41f00a6b3c12b3b64ec4d97be0453b
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2025-01-15 22:56:56 +0100

    final build

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 from main import create_app
-from config import DevConfig
+from config import DevConfig,ProdConfig
 
 if __name__ == "__main__":
-    app = create_app(DevConfig)
-    app.run()
+    app = create_app(ProdConfig)
