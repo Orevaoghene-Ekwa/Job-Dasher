dd321b9e0d3fc423e0a9ec3dee60d608c9301a3d
Orevaoghene Ekwa <ekwaorevaoghene@gmail.com>
2025-01-18 10:23:21 +0100

    reinitialised db migration

--- migrations/versions/9e5cb8f25c31_initial_migration.py
+++ migrations/versions/9e5cb8f25c31_initial_migration.py
@@ -1,8 +1,8 @@
-"""empty message
+"""initial migration
 
-Revision ID: 743f8e5d9614
+Revision ID: 9e5cb8f25c31
 Revises: 
-Create Date: 2025-01-18 10:09:58.943009
+Create Date: 2025-01-18 10:22:43.853823
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '743f8e5d9614'
+revision = '9e5cb8f25c31'
 down_revision = None
 branch_labels = None
 depends_on = None
